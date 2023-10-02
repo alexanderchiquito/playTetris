e738c83fc34fdea8489728cbc505b327bb25e632
Alexander Ramírez Chiquito <alexander.ramirezc@unac.edu.co>
2023-10-02 17:18:23 -0500

    fix functional

---
+++ tetris-rush/main.js
@@ -0,0 +1,201 @@
+import './style.css'
+
+// 1. Inicializar el canvas
+
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+const BLOCK_SIZE = 20
+const BOARD_WIDTH = 14
+const BOARD_HEIGHT = 30
+
+canvas.width = BLOCK_SIZE * BOARD_WIDTH
+canvas.height = BLOCK_SIZE * BOARD_HEIGHT
+
+context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+const board = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1]
+]
+
+// 4 player
+
+const piece = {
+  position: { x: 5, y: 5 },
+  shape: [
+    [1, 1],
+    [1, 1]
+  ]
+}
+
+const PIECES = [
+  [
+    [1, 1],
+    [1, 1]
+  ],
+  [
+    [1, 1, 1, 1]
+  ],
+  [
+    [0, 1, 0],
+    [1, 1, 1]
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1]
+  ],
+  [
+    [1, 0],
+    [1, 0],
+    [1, 1]
+  ]
+]
+
+// 2. Game loop
+
+// function update () {
+//   draw()
+//   window.requestAnimationFrame(update)
+// }
+
+// 8. Auto Drop
+
+let dropCounter = 0
+let lastTime = 0
+
+function update (time = 0) {
+  const deltaTime = time - lastTime
+  lastTime = time
+
+  dropCounter += deltaTime
+
+  if (dropCounter > 1000) {
+    piece.position.y++
+    dropCounter = 0
+    if (checkCollision()) {
+      piece.position.y--
+      solidifypiece()
+      removeRows()
+    }
+  }
+
+  draw()
+  window.requestAnimationFrame(update)
+}
+
+function draw () {
+  context.fillStyle = '#f9f9f9'
+  context.fillRect(0, 0, canvas.width, canvas.height)
+
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        context.fillStyle = 'yellow'
+        context.fillRect(x, y, 1, 1)
+      }
+    })
+  })
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value) {
+        context.fillStyle = 'red'
+        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1)
+      }
+    })
+  })
+}
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'ArrowLeft') {
+    piece.position.x--
+    if (checkCollision()) {
+      piece.position.x++
+    }
+  }
+  if (event.key === 'ArrowRight') {
+    piece.position.x++
+    if (checkCollision()) {
+      piece.position.x--
+    }
+  }
+  if (event.key === 'ArrowDown') {
+    piece.position.y++
+    if (checkCollision()) {
+      piece.position.y--
+      solidifypiece()
+      removeRows()
+    }
+  }
+})
+
+function checkCollision () {
+  return piece.shape.find((row, y) => {
+    return row.find((value, x) => {
+      return (
+        value !== 0 &&
+        board[y + piece.position.y]?.[x + piece.position.x] !== 0
+      )
+    })
+  })
+}
+
+function solidifypiece () {
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        board[y + piece.position.y][x + piece.position.x] = 1
+      }
+    })
+  })
+  // get random shape
+  piece.shape = PIECES[Math.floor(Math.random() * PIECES.length)]
+  // RESET Position
+  piece.position.x = 0
+  piece.position.y = 0
+}
+
+function removeRows () {
+  const rowsToRemove = []
+
+  board.forEach((row, y) => {
+    if (row.every(value => value === 1)) {
+      rowsToRemove.push(y)
+    }
+  })
+  rowsToRemove.forEach(y => {
+    board.splice(y, 1)
+    const newRow = Array(BOARD_WIDTH).fill(0)
+    board.unshift(newRow)
+  })
+}
+
+update()
